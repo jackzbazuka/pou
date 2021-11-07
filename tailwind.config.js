fffbe85630a4e6e7c916dc384d5fb098ca200b0b
jackzbazuka <milindmoto99@gmail.com>
2021-11-07 19:43:58 +0530

    extended css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: { ...colors },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	mode: 'jit',
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: { ...colors },
+			height: {
+				sul: '350px',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
